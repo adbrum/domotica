e9d4835d77b7b40b92e9b2ee4d1611bf2c839633
Adriano Regis <adbrum@outlook.com>
2019-04-03 14:52:34 +0100

    Add code MQTT

--- domotica/core/views.py
+++ domotica/core/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
+import time
+import paho.mqtt.client as paho
 
+#define callback
+def on_message(client, userdata, message):
+    time.sleep(1)
+    print("received message =",str(message.payload.decode("utf-8")))
+
+def clientConnect(state):
+    broker="192.168.1.46"
+    client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+    ######Bind function to callback
+    client.on_message=on_message
+    #####
+    print("connecting to broker ",broker)
+    client.connect(broker)#connect
+    client.loop_start() #start loop to process received messages
+    print("subscribing ")
+    client.subscribe("test")#subscribe
+    time.sleep(1)
+    print("publishing ")
+    client.publish("test",state)#publish
+    time.sleep(1)
+    client.disconnect() #disconnect
+    client.loop_stop() #stop loop
 
 def index(request):
     return render(request, 'core/index.html')
 
 
 def lighting(request):
+    #clientConnect()
+    state = request.GET.get('state')
+    print(state)
+    clientConnect(state)
     return render(request, 'core/lighting.html')
 
 
@@ -23,3 +51,7 @@ def cam01(request):
 
 # def index(request):
 #   return HttpResponse("Hello, world. You're at the polls index.")
+
+
+
+
