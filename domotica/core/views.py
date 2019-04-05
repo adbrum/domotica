f830e4b6a9be2326142770e48a93f72fc326b8c3
Adriano Regis <adbrum@outlook.com>
2019-04-05 14:49:45 +0100

    Adicionado GPIO (17) read

--- domotica/core/views.py
+++ domotica/core/views.py
@@ -1,34 +1,44 @@
 from django.shortcuts import render
 import time
 import paho.mqtt.client as paho
+from gpiozero import LED
 
-#define callback
+
+# define callback
 def on_message(client, userdata, message):
     time.sleep(1)
-    print("received message =",str(message.payload.decode("utf-8")))
+    print("received message =", str(message.payload.decode("utf-8")))
+
 
 def clientConnect(request):
     state = request.GET.get('state')
     place = request.GET.get('place')
-    print('STATE: ',state)
+    print('STATE: ', state)
     print('PLACE: ', place)
-    broker="192.168.1.46"
-    client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
-    ######Bind function to callback
-    client.on_message=on_message
+    broker = "192.168.1.46"
+    # create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+    client = paho.Client("client-001")
+    # Bind function to callback
+    client.on_message = on_message
     #####
-    print("connecting to broker ",broker)
-    client.connect(broker)#connect
-    client.loop_start() #start loop to process received messages
+    print("connecting to broker ", broker)
+    client.connect(broker)  # connect
+    client.loop_start()  # start loop to process received messages
     print("subscribing ")
-    client.subscribe(place)#subscribe
+    client.subscribe(place)  # subscribe
+    if place == 'sala':
+        led = LED(17)
+        led.on()
+    else:
+        pass
     time.sleep(1)
     print("publishing ")
-    client.publish(place, state)#publish
+    client.publish(place, state)  # publish
     time.sleep(1)
-    client.disconnect() #disconnect
-    client.loop_stop() #stop loop
+    client.disconnect()  # disconnect
+    client.loop_stop()  # stop loop
     return render(request, 'core/index.html')
+
 
 def index(request):
     return render(request, 'core/index.html')
@@ -47,7 +57,3 @@ def surveillance(request):
 def cam01(request):
     print(request)
     return render(request, 'core/cam_01.html')
-
-
-
-
