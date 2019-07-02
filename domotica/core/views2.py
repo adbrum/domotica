7ed20c9c861440a1cc1c9833996e21cdc9d1b843
adbrum <adbrum@outlook.com>
2019-07-02 22:44:56 +0000

    alterações carlos

--- domotica/core/views2.py
+++ domotica/core/views2.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 import time
 from gpiozero import LED
 
+blind_time = 10 #Time to open completely the blind
+blind_pos = 0
+
 light_1 = LED(16)
 light_2 = LED(20)
 light_3 = LED(21)
@@ -9,56 +12,66 @@ light_3 = LED(21)
 blind_1up = LED(17)
 blind_1dw = LED(18)
 
-blind_pos = 0
 
 def on_message(client, userdata, message):
     time.sleep(1)
     print("received message =", str(message.payload.decode("utf-8")))
+
 
 def clientConnect(request):
     state = request.GET.get('state')
     place = request.GET.get('place')
     print('STATE: ', state)
     print('PLACE: ', place)
-
-    if place == 'sala':
+     
+  #  blind_p = blind_pos
+     
+   
+    if place   == 'sala':
         light_1.toggle()
     elif place == 'quarto':
         light_2.toggle()
     elif place == 'cozinha':
         light_3.toggle()
-
+    
     elif place == 'estore_sala':
-        global blind_pos
-
-        if (state == 'true' and blind_pos < 100)  and blind_1dw.value == 0:
+             
+        
+        if state == 'true' and blind_1dw.value == 0:
             blind_1up.on()
             time.sleep(2)
             blind_1up.off()
-            blind_pos = (blind_pos + 25)
-
-
-        elif (state == 'false' and blind_pos > 0) and blind_1up.value == 0:
+       #     blind_p = (blind_p + 25)
+       #     print('test gpio = ', blind_1up.value)
+            
+        elif state =='false' and blind_1up.value == 0: 
             blind_1dw.on()
             time.sleep(2)
             blind_1dw.off()
-            blind_pos = (blind_pos - 25)
-
+     #       blind_p = (blind_p - 25)
+            
+        
+    #    blind_pos = blind_p
         print('BLIND = ', blind_pos)
-
+        
+             
     time.sleep(1)
     return render(request, 'core/index.html')
 
+
 def index(request):
     return render(request, 'core/index.html')
+
 
 def lighting(request):
     print(request)
     return render(request, 'core/lighting.html')
 
+
 def surveillance(request):
     print(request)
     return render(request, 'core/surveillance.html')
+
 
 def cam01(request):
     print(request)
--- domotica/core/views_old.py
+++ domotica/core/views_old.py
@@ -2,18 +2,14 @@ from django.shortcuts import render
 import time
 from gpiozero import LED
 
-light_1 = LED(16)
-light_2 = LED(20)
-light_3 = LED(21)
-
-blind_1up = LED(17)
-blind_1dw = LED(18)
-
-blind_pos = 0
+led1 = LED(20)
+led2 = LED(21)
+led3 = LED(16)
 
 def on_message(client, userdata, message):
     time.sleep(1)
     print("received message =", str(message.payload.decode("utf-8")))
+
 
 def clientConnect(request):
     state = request.GET.get('state')
@@ -22,43 +18,28 @@ def clientConnect(request):
     print('PLACE: ', place)
 
     if place == 'sala':
-        light_1.toggle()
+        led1.toggle()
     elif place == 'quarto':
-        light_2.toggle()
+        led2.toggle()
     elif place == 'cozinha':
-        light_3.toggle()
-
-    elif place == 'estore_sala':
-        global blind_pos
-
-        if (state == 'true' and blind_pos < 100)  and blind_1dw.value == 0:
-            blind_1up.on()
-            time.sleep(2)
-            blind_1up.off()
-            blind_pos = (blind_pos + 25)
-
-
-        elif (state == 'false' and blind_pos > 0) and blind_1up.value == 0:
-            blind_1dw.on()
-            time.sleep(2)
-            blind_1dw.off()
-            blind_pos = (blind_pos - 25)
-
-        print('BLIND = ', blind_pos)
-
+        led3.toggle()
     time.sleep(1)
     return render(request, 'core/index.html')
 
+
 def index(request):
     return render(request, 'core/index.html')
+
 
 def lighting(request):
     print(request)
     return render(request, 'core/lighting.html')
 
+
 def surveillance(request):
     print(request)
     return render(request, 'core/surveillance.html')
+
 
 def cam01(request):
     print(request)
